55d042245accbf7c2b5ed4a12ba55dcc2e978671
Owen Synge <owen.synge@jaysnest.de>
2017-11-10 01:27:45 +0100

    First draft of autotools
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

---
+++ autogen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -x
+(git rev-parse HEAD ; git describe) 2> /dev/null > src/githash
+if [ ! -d "config" ] ; then
+mkdir config
+fi
+if [ ! -d "m4" ] ; then
+mkdir m4
+fi
+aclocal -I config
+autoheader
+libtoolize --automake
+touch AUTHORS
+touch ChangeLog
+touch NEWS
+touch README
+touch COPYING
+touch INSTALL
+automake --add-missing --copy
+autoconf
+
